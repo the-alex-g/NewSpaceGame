2c0d1539e55c8884c7d917e019d41ef4f01f7093
Alex G <alex.gestwicki@gmail.com>
2025-05-15 12:21:12 -0400

    Add repairing

--- project/player/player.gd
+++ project/player/player.gd
@@ -12,13 +12,20 @@ var _thrust := 0 :
 			_thrust = floor(fuel / thrust_fuel_burn_multiplier)
 		return _thrust
 var max_thrust := 6
-var fuel := 0.0 :
+var fuel := 100.0 :
 	set(value):
 		if value >= 0.0 and value <= max_fuel:
 			fuel = value
 var max_fuel := 100.0
 var fuel_regeneration := 10.0
 var thrust_fuel_burn_multiplier := 2.5
+var health := 50.0 :
+	set(value):
+		if value <= max_health and value >= 0.0:
+			health = value
+var max_health := 100.0
+var repair_fuel_cost := 2.0
+var max_repair_per_second := 10.0
 
 
 func _physics_process(delta: float) -> void:
@@ -31,8 +38,18 @@ func _physics_process(delta: float) -> void:
 	rotate(Vector3.UP, turning * delta * TAU * turn_speed)
 	$Camera3D.global_rotation = Vector3(-PI/2, 0.0, 0.0)
 	
-	fuel += fuel_regeneration * delta
-	fuel -= _thrust * delta * thrust_fuel_burn_multiplier
-	print(fuel)
+	var delta_fuel := fuel_regeneration - _thrust * thrust_fuel_burn_multiplier
+	
+	if health < max_health and delta_fuel > 0.0:
+		var repair_amount := minf(
+			max_repair_per_second,
+			floor(delta_fuel / repair_fuel_cost)
+		)
+		health += repair_amount * delta
+		delta_fuel -= repair_fuel_cost * repair_amount
+	
+	fuel += delta_fuel * delta
+	
+	print("ship ", index, ": fuel ", round(fuel), " health ", round(health))
 	
 	move_and_collide(Vector3.FORWARD.rotated(Vector3.UP, rotation.y) * delta * _thrust * 50)
